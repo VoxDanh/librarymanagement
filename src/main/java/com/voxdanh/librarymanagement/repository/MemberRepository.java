c29043c635a8e732d82a11c9064c2b9f9cc8d445
VoxDanh <vcongdanh8686@gmail.com>
2026-08-27 19:18:03 +0700

    Thêm memberService

--- src/main/java/com/voxdanh/librarymanagement/repository/MemberRepository.java
+++ src/main/java/com/voxdanh/librarymanagement/repository/MemberRepository.java
@@ -2,6 +2,14 @@ package com.voxdanh.librarymanagement.repository;
 
 import com.voxdanh.librarymanagement.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
-public interface MemberRepository extends JpaRepository<Member,Long> {
 
+import java.time.LocalDate;
+import java.util.List;
+
+public interface MemberRepository extends JpaRepository<Member,Long> {
+    List<Member> findByNameContainingIgnoreCase(String name);
+    List<Member> findByPhoneContainingIgnoreCase(String phone);
+    List<Member> findByEmailContainingIgnoreCase(String email);
+    List<Member> findByGender(Boolean gender);
+    List<Member> findByBirthDateBetween(LocalDate start, LocalDate end);
 }
--- src/main/java/com/voxdanh/librarymanagement/service/MemberService.java
+++ src/main/java/com/voxdanh/librarymanagement/service/MemberService.java
@@ -1,4 +1,45 @@
 package com.voxdanh.librarymanagement.service;
 
+import com.voxdanh.librarymanagement.model.Member;
+import com.voxdanh.librarymanagement.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public class MemberService {
+    @Autowired
+    MemberRepository memberRepository;
+    public Member addMember(String name, LocalDate birthDate,Boolean gender,String phone,String email){
+        Member newMember=new Member();
+        newMember.setFullName(name);
+        newMember.setBirthDate(birthDate);
+        newMember.setGender(gender);
+        newMember.setPhone(phone);
+        newMember.setEmail(email);
+        newMember=memberRepository.save(newMember);
+        return newMember;
+    }
+
+    public void deleteMember(Long id){
+        memberRepository.deleteById(id);
+    }
+
+
+
+    public List<Member> findByName(String name){
+        return memberRepository.findByNameContainingIgnoreCase(name);
+    }
+    public List<Member> findByEmail(String email){
+        return memberRepository.findByEmailContainingIgnoreCase(email);
+    }
+    public List<Member> findByPhone(String phone){
+        return memberRepository.findByPhoneContainingIgnoreCase(phone);
+    }
+    public List<Member> findByGender(Boolean gender){
+        return memberRepository.findByGender(gender);
+    }
+    public List<Member> findByBirthDateBetween(LocalDate start,LocalDate end){
+        return memberRepository.findByBirthDateBetween(start,end);
+    }
 }
